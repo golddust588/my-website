3cbd1188c2ff8de2464204349c991d41f1d275a3
golddust588 <124166610+golddust588@users.noreply.github.com>
2024-09-28 12:31:22 +0300

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,11 @@ const Footer = () => {
     return new Date().getFullYear().toString();
   };
   return (
-    <div className="text-sm text-gray-400 h-12 min-w-full flex justify-center items-center gap-6 bg-dark-background mt-10">
-      <span>&copy; {getCurrentYear()} Algimantas Skara</span>
-      <span>Full stack developer</span>
+    <div className="text-sm text-gray-400 h-12 min-w-[400px] w-full flex justify-center items-center gap-6 bg-dark-background mt-10">
+      <div className="flex gap-4">
+        <span>&copy; {getCurrentYear()} Algimantas Skara</span>
+        <span>Full stack developer</span>
+      </div>
     </div>
   );
 };
