6acf730b75b96e0c81e0bb8253b4352980cbf363
golddust588 <124166610+golddust588@users.noreply.github.com>
2024-09-30 18:58:55 +0300

    new text

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,11 +3,11 @@ const AboutMe = () => {
     <div>
       <h1 className="text-light-purple">About me:</h1>
       <p className="mt-2 leading-loose">
-        Although I hold a master's degree in International Economics, I never
-        found much joy in it. Later, I discovered programming and instantly dove
-        deep into it. I enjoy both creating new projects and maintaining
-        existing ones, finding great satisfaction in generating significant
-        economic value through technology.
+        Although I hold a master&#39;s degree in International Economics, I
+        never found much joy in it. Later, I discovered programming and
+        instantly dove deep into it. I enjoy both creating new projects and
+        maintaining existing ones, finding great satisfaction in generating
+        significant economic value through technology.
       </p>
       <div className="flex items-center gap-4 mt-4">
         <h1 className="text-6xl font-bold text-light-purple">1.5+</h1>
