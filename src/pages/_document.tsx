fa0ed087fdf1c1b53ba4ef453a7e1c022b19d44e
golddust588 <124166610+golddust588@users.noreply.github.com>
2024-10-02 11:34:47 +0300

    added meta to Head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Full stack developer | Algimantas Skara</title>
+        <meta
+          name="description"
+          content="Portfolio website of a skilled full-stack developer specializing in modern web development. Available for freelance or full-time hire. Explore projects and contact for job opportunities."
+        />
+        <meta name="keywords" content="full-stack, developer, react" />
+      </Head>
       <body className="bg-main-background text-white">
         <Main />
         <NextScript />
