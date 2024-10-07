34befa5f5eea1c30d308120932cf912d8645b215
golddust588 <124166610+golddust588@users.noreply.github.com>
2024-10-07 20:49:10 +0300

    Head improved

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,18 @@ export default function Document() {
           content="Portfolio website of a skilled full-stack React/Node.js developer specializing in modern web development. Available for freelance or full-time hire. Explore projects and contact for job opportunities."
         />
         <meta name="keywords" content="full-stack, developer, react" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Person",
+              name: "Algimantas Skara",
+              jobTitle: "Full Stack Developer",
+              url: "https://www.algimantas-skara.com",
+            }),
+          }}
+        />
       </Head>
       <body className="bg-main-background text-white">
         <Main />
